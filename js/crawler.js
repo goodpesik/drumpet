edbe519c554390e8502f63935aa42152346c6f15
Maksym.Levynets <maksym.levynets@sophos.com>
2019-01-15 11:04:12 +0200

    changes

--- js/crawler.js
+++ js/crawler.js
@@ -1,90 +1,114 @@
 const express = require('express');
-const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
 const app     = express();
-const FeedParser = require('feedparser');
+const feedImages = 8;
 
-const feedKey = "временно прекращено водоснабжение";
+app.get('/instaFeed', function(req, res) {
+	setHeaders(res);
+	getInstaFeed('https://www.instagram.com/drumpetoficial/').then(function(data) {
+		res.json(data);
+	})
+});
 
+app.get('/events', function(req, res) {
+	setHeaders(res);
+	getEvents('https://docs.google.com/spreadsheets/d/e/2PACX-1vTo4WQ3e98bz9sN18AfOyVJ3-Z7mg-LxSZHB57NAgKCsd22cdpCNvaHZIXhDxb1tUxEO7YUTYSY-7sj/pubhtml').then(function(data) {
+		res.json(data);
+	})
+});
 
-
-app.get('/', function(req, res) {
-
-	url = 'https://www.instagram.com/goodpesik__music/';
-
-	request(url, function(error, response, html){
-		if(!error){
-			let $ = cheerio.load(html);
-			let dataString = $('body script').eq(0).html();
-			const dataStart = "window._sharedData = ";
-			const dataEnd = ";"
-			let data;
-			let instagramFeed = [];
-
-			dataString = dataString.replace(dataStart,'');
-			dataString = dataString.replace(dataEnd,'');
-			data = JSON.parse(dataString).entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges;
-			
-				for (let index = 0; index < data.length; index++) {
+function setHeaders(res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+}
+	
+function getInstaFeed(url) {
+	return new Promise(function(resolve, reject) {
+		request(url, function(error, response, html) {
+			if(!error) {
+				let $ = cheerio.load(html);
+				let dataString = $('body script').eq(0).html();
+				const dataStart = "window._sharedData = ";
+				const dataEnd = ";"
+				let data;
+				let instagramFeed = [];
+	
+				dataString = dataString.replace(dataStart,'');
+				dataString = dataString.replace(dataEnd,'');
+				data = JSON.parse(dataString).entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges;
+				
+				for (let index = 0; index < data.length && index < feedImages; index++) {
 					instagramFeed.push({
 						scr: data[index].node.display_url,
 						text: data[index].node.edge_media_to_caption.edges[0].node.text,
 						url: 'instagram.com/p/'+data[index].node.shortcode
 					});
 				}
-			
-			console.log(instagramFeed);
+				resolve(instagramFeed);
+			} else {
+				reject(error);
 			}
-	})
-	
-	// rss();
-	
-	// function rss() {
-	// 	let rssData = [];
-	// 	let feedparser = new FeedParser();
-	// 	var req = request('https://vodokanal.kharkov.ua/rss');
-		
-	// 	req.on('response', function (res) {
-	// 		var stream = this;
+		})
+	});
+}
 
-	// 		if (res.statusCode !== 200) {
-	// 			this.emit('error', new Error('Bad status code'));
-	// 		}
-	// 		else {
-	// 			stream.pipe(feedparser);
-	// 		}
-	// 	});
-		
-	// 	feedparser.on('readable', function () {
-	// 		let stream = this; 
-	// 		let item;
-	// 		let content = '';
-	// 		const templateFirst = '\n<b>';
-	// 		const templateSecond = '</b>\n';
-			
-	// 		while (item = stream.read()) {
-	// 			if (item.title.indexOf(feedKey) !== -1) {
-	// 				let content = '';
-	// 				let feedDate = item['rss:pubdate']['#'].replace('+0300','');
-	// 				content += templateFirst + item['rss:pubdate']['#'].replace('+0300','') + templateSecond;
-	// 				content += item['yandex:full-text']['#'];
-					
-	// 				rssData.push(content);
-	// 			}
-	// 		}
-			
-	// 	});
-		
-	// 	feedparser.on('end', function() {
-	// 		console.log(rssData);
-	// 	});
-	// }
+function getEvents(url) {
+	return new Promise(function(resolve, reject) {
+		request(url, function(error, response, html) {
+			if(!error) {
+				let $ = cheerio.load(html);
+				let dataObj = {};
+				let data = [];
+				let dataTable = $('table tbody tr');
+				let dataKeys = dataTable.eq(0).find('td');
 
+				if (dataKeys.length) {
+					dataKeys.each(function(index,item){
+						dataObj[item.children[0].data] = '';
+					});
+				}
 
-});
-	
-	
+				for (let index = 1; index < dataTable.length; index++) {
+					let rowData = dataTable.eq(index).find('td');
+					let keyNumber = 0;
+					let rowDataArr = [];
+					let tempObj = {};
+
+					rowData.each(function(index,item){
+						
+						if (item.children.length) {
+							if (item.children[0].type === 'tag') {
+								if (item.children[0].name === 'a') {
+									rowDataArr.push(item.children[0].children[0].data);
+								} else {
+									rowDataArr.push(item.children[0].children[0].children[0].data);
+								}
+							} else {
+								rowDataArr.push(item.children[0].data);
+							}
+						}
+					});
+
+					for (const key in dataObj) {
+						if (dataObj.hasOwnProperty(key)) {
+							if (keyNumber > rowDataArr.length) {
+								tempObj[key] = '';
+							} else {
+								tempObj[key] = rowDataArr[keyNumber];
+							}
+							keyNumber++;
+						}
+					}
+
+					data.push(tempObj);
+				}
+
+				resolve(data);
+			}
+		});
+	});
+}
 
 app.listen('8081');
 console.log('Magic happens on port 8081');
--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ window.onload = function() {
 function init() {
     facebookIframe();
     langSelect();
+    instaFeed();
+    getEvents();
 }
 
 function facebookIframe() {
@@ -35,4 +37,45 @@ function langSelect() {
 
 function toggleClass(element, className) {
     element.classList.toggle(className);
+}
+
+function instaFeed() {
+    let container = document.querySelector('.instaFeed');
+    let loadClass = 'loading';
+    container.classList.add(loadClass);
+    httpGetAsync('http://localhost:8081/instaFeed').then(function(resp) {
+        container.classList.remove(loadClass);
+        let containerHtml = '';
+        if (resp.length) {
+            containerHtml = `
+            ${resp.map(function(item){
+                return `
+                    <a class="post" href="https//${item.url}">
+                        <img src="${item.scr}" alt="${item.text}">
+                        <p>${item.text}</p>
+                    </a>
+                `
+                }).join('')}
+            `;
+        }
+        container.innerHTML = containerHtml;
+    });
+}
+
+function getEvents() {
+    httpGetAsync('http://localhost:8081/events').then(function(resp) {
+        console.log(resp);
+    });
+}
+
+function httpGetAsync(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    return new Promise(function(resolve, reject) {
+        xmlHttp.onload = function(resp) {
+            resolve(JSON.parse(resp.currentTarget.response));
+        }
+
+        xmlHttp.open("GET", theUrl, true);
+        xmlHttp.send();
+	});
 }
